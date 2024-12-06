3d1ce26f357611e68c7fab8c6fc916e7be51af28
gjsk132 <gjsk132@naver.com>
2024-12-07 05:00:31 +0900

    [Feature] Add click event

--- app/src/main/java/com/gjsk/lootify/title/StartTitle.java
+++ app/src/main/java/com/gjsk/lootify/title/StartTitle.java
@@ -2,15 +2,23 @@ package com.gjsk.lootify.title;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.gjsk.lootify.R;
+import com.gjsk.lootify.databinding.ActivityStartTitleBinding;
 
 public class StartTitle extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_title);
+
+        com.gjsk.lootify.databinding.ActivityStartTitleBinding binding = ActivityStartTitleBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startScreen.setOnClickListener(v->{
+            Intent intent = new Intent(StartTitle.this, Lobby.class);
+            startActivity(intent);
+        });
     }
 }
