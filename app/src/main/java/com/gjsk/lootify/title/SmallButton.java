ff4eb495c4f71f1ac76547c23e64f141dcabee84
gjsk132 <gjsk132@naver.com>
2024-12-07 06:57:27 +0900

    [Feature] Update SmallButton CustomView

--- app/src/main/java/com/gjsk/lootify/title/SmallButton.java
+++ app/src/main/java/com/gjsk/lootify/title/SmallButton.java
@@ -2,6 +2,7 @@ package com.gjsk.lootify.title;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.gjsk.lootify.R;
+
+import java.util.Objects;
 
 public class SmallButton extends FrameLayout {
     TextView text;
@@ -56,12 +59,66 @@ public class SmallButton extends FrameLayout {
     private void setTypeArray(TypedArray typedArray) {
 
         String textString = typedArray.getString(R.styleable.SmallButton_text);
-        text.setText(textString);
+        setText(textString);
 
         int textColor = typedArray.getColor(R.styleable.SmallButton_text_color, 0);
-        text.setTextColor(textColor);
+        setTextColor(textColor);
+
+        boolean buttonEnable = typedArray.getBoolean(R.styleable.SmallButton_enable, false);
+        setEnabled(buttonEnable);
 
         typedArray.recycle();
     }
 
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        smallButton.setEnabled(enabled);
+        smallButton.setAlpha(enabled ? 1f : 0.7f);
+    }
+
+    public void setText(String textString) {
+        text.setText(textString);
+        smallButton.setBackgroundResource(setButtonColor(textString));
+    }
+
+    private void setTextColor(int textColor) {
+        text.setTextColor(textColor);
+    }
+
+    private int setButtonColor(String textString){
+
+        switch (Objects.requireNonNull(textString)){
+            case "FIND":
+            case "SELECT":
+            case "NEXT" :
+                return R.drawable.light_yellow_button;
+
+            case "JOIN":
+            case "LOAD":
+            case "SAVE" :
+                return R.drawable.light_green_button;
+
+            case "CANCLE":
+            case "BEFORE":
+            case "BACK":
+                return R.drawable.gray_button;
+
+            case "SAVE MAP":
+            case "CREATE MAP":
+                return R.drawable.light_black_button;
+
+            case "MY MAP":
+                return R.drawable.light_brown_button;
+
+            case "HISTORY":
+                return R.drawable.dark_yellow_button;
+
+            case "PREVIEW":
+                return R.drawable.mid_green_button;
+
+            default:
+                return R.drawable.white_frame;
+        }
+    }
 }
